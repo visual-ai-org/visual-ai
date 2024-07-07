79be4343a7c2e9c810798358e3f2fbc0d37435dc
Chustinjeng <justcheng2000@gmail.com>
2024-07-07 16:48:52 +0800

    Make refresh button

--- frontend/src/ControlPanel.js
+++ frontend/src/ControlPanel.js
@@ -1,13 +1,18 @@
 import {
-  Button, Dialog, DialogActions,
-  DialogContent, DialogContentText, DialogTitle,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
   Divider,
   List,
   ListItem,
   ListItemButton,
   ListItemIcon,
   ListItemText,
-  Stack, TextField,
+  Stack,
+  TextField,
   Toolbar,
   Typography,
 } from "@mui/material";
@@ -16,17 +21,27 @@ import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
 import { useEffect, useState } from "react";
 import Layer from "./Layer";
-import {setTrainData} from "./api";
+import { setTrainData } from "./api";
+import { WindowSharp } from "@mui/icons-material";
 
 const drawerWidth = 400;
 const layerLimit = 6;
 
-export default function ControlPanel({handleTraining, setIsTraining, training, learningRate, epochs, setLearningRate, setEpochs}) {
+export default function ControlPanel({
+  handleTraining,
+  setIsTraining,
+  training,
+  learningRate,
+  epochs,
+  setLearningRate,
+  setEpochs,
+}) {
   const [layers, setLayers] = useState(0);
   const [open, setOpen] = useState(false);
-  const [trainData, setTrainDataInput] = useState('[[0, 0], [0, 1], [1, 0], [1, 1]]');
-  const [labels, setLabelsInput] = useState('[[0], [1], [1], [0]]');
-
+  const [trainData, setTrainDataInput] = useState(
+    "[[0, 0], [0, 1], [1, 0], [1, 1]]"
+  );
+  const [labels, setLabelsInput] = useState("[[0], [1], [1], [0]]");
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -84,108 +99,112 @@ export default function ControlPanel({handleTraining, setIsTraining, training, l
     console.log(`number of layers is ${layers}`);
   };
 
+  const handleRefresh = () => {
+    window.location.reload();
+  };
+
   return (
-      <Drawer
-          sx={{
-            width: drawerWidth,
-            flexShrink: 0,
-            "& .MuiDrawer-paper": {
-              width: drawerWidth,
-              boxSizing: "border-box",
-              backgroundColor: '#FFF2D7',
-            },
-          }}
-          variant="permanent"
-          anchor="right"
-      >
-        <Button onClick={handleClickOpen}>
-          Set Training Data
-        </Button>
-        <Dialog open={open} onClose={handleClose}>
-          <DialogTitle>Set Training Data</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Please enter the training data and labels in JSON format, as well as the learning rate and epochs.
-            </DialogContentText>
-            <TextField
-                autoFocus
-                margin="dense"
-                id="trainData"
-                label="Training Data"
-                type="text"
-                fullWidth
-                variant="standard"
-                value={trainData}
-                onChange={(e) => setTrainDataInput(e.target.value)}
-            />
-            <TextField
-                margin="dense"
-                id="labels"
-                label="Labels"
-                type="text"
-                fullWidth
-                variant="standard"
-                value={labels}
-                onChange={(e) => setLabelsInput(e.target.value)}
-            />
-            <TextField
-                margin="dense"
-                id="learningRate"
-                label="Learning Rate"
-                type="float"
-                fullWidth
-                variant="standard"
-                value={learningRate}
-                onChange={(e) => setLearningRate(e.target.value)}
-            />
-            <TextField
-                margin="dense"
-                id="epochs"
-                label="Epochs"
-                type="number"
-                fullWidth
-                variant="standard"
-                value={epochs}
-                onChange={(e) => setEpochs(e.target.value)}
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose}>Cancel</Button>
-            <Button onClick={handleSubmit}>Submit</Button>
-          </DialogActions>
-        </Dialog>
+    <Drawer
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        "& .MuiDrawer-paper": {
+          width: drawerWidth,
+          boxSizing: "border-box",
+          backgroundColor: "#FFF2D7",
+        },
+      }}
+      variant="permanent"
+      anchor="right"
+    >
+      <Button onClick={handleClickOpen}>Set Training Data</Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Set Training Data</DialogTitle>
         <DialogContent>
-          <Toolbar variant="regular">
-            <Typography variant="h7" color="inherit" component="div">
-              Configure layers and perceptrons
-            </Typography>
-          </Toolbar>
-          <Divider/>
-          <List>
-            {Array.from({length: layers}, (_, index) => (
-                <Layer index={index} lastIndex={layers - 1}> </Layer>
-            ))}
-          </List>
+          <DialogContentText>
+            Please enter the training data and labels in JSON format, as well as
+            the learning rate and epochs.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="trainData"
+            label="Training Data"
+            type="text"
+            fullWidth
+            variant="standard"
+            value={trainData}
+            onChange={(e) => setTrainDataInput(e.target.value)}
+          />
+          <TextField
+            margin="dense"
+            id="labels"
+            label="Labels"
+            type="text"
+            fullWidth
+            variant="standard"
+            value={labels}
+            onChange={(e) => setLabelsInput(e.target.value)}
+          />
+          <TextField
+            margin="dense"
+            id="learningRate"
+            label="Learning Rate"
+            type="float"
+            fullWidth
+            variant="standard"
+            value={learningRate}
+            onChange={(e) => setLearningRate(e.target.value)}
+          />
+          <TextField
+            margin="dense"
+            id="epochs"
+            label="Epochs"
+            type="number"
+            fullWidth
+            variant="standard"
+            value={epochs}
+            onChange={(e) => setEpochs(e.target.value)}
+          />
         </DialogContent>
-        <List
-            style={{position: "absolute", bottom: "0"}}
-            component={Stack}
-            direction={"row"}
-            spacing={"130px"}
-            sx={{padding: "50px"}}
-        >
-          <ListItemButton onClick={handleAddClick}>
-            <ListItemIcon>
-              <AddIcon fontSize="large"/>
-            </ListItemIcon>
-          </ListItemButton>
-          <ListItemButton onClick={handleRemoveClick}>
-            <RemoveIcon fontSize="large"/>
-          </ListItemButton>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleSubmit}>Submit</Button>
+        </DialogActions>
+      </Dialog>
+      <DialogContent>
+        <Toolbar variant="regular">
+          <Typography variant="h7" color="inherit" component="div">
+            Configure layers and perceptrons
+          </Typography>
+        </Toolbar>
+        <Divider />
+        <List>
+          {Array.from({ length: layers }, (_, index) => (
+            <Layer index={index} lastIndex={layers - 1}>
+              {" "}
+            </Layer>
+          ))}
         </List>
-        <Button onClick={handleTraining}>
-          Start Training
-        </Button>
-      </Drawer>
+      </DialogContent>
+      <List
+        style={{ position: "absolute", bottom: "0" }}
+        component={Stack}
+        direction={"row"}
+        spacing={"130px"}
+        sx={{ padding: "50px", paddingBottom: "100px" }}
+      >
+        <ListItemButton onClick={handleAddClick}>
+          <ListItemIcon>
+            <AddIcon fontSize="large" />
+          </ListItemIcon>
+        </ListItemButton>
+        <ListItemButton onClick={handleRemoveClick}>
+          <RemoveIcon fontSize="large" />
+        </ListItemButton>
+      </List>
+      <Button onClick={handleTraining}>Start Training</Button>
+      {!training ? <Button onClick={handleRefresh}>Refresh</Button> : null}
+    </Drawer>
   );
 }
